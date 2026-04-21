7869d87201a1c3f861dff6e8915b74703d9ea9da
moeyan-gmi <moeyanoo.ged@gmail.com>
2026-04-22 00:55:38 +0700

    feat(api-request): accept client request and parse that request to get method

--- server.c
+++ server.c
@@ -1,8 +1,21 @@
 #include <netinet/in.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 
 #define port 9090
+#define BUFFER_SIZE 1024
+
+void getMethod(int socketfd, char *buff, char *method) {
+  int request = recv(socketfd, buff, BUFFER_SIZE, 0);
+  if (request <= 0) {
+    printf("Didn't receive any data in requests\n");
+  }
+  printf("Buffer is %s\n", buff);
+
+  sscanf(buff, "%15s", method);
+  printf("Method: %s\n", method);
+}
 
 int main() {
   printf("Program starts\n");
@@ -11,7 +24,6 @@ int main() {
     printf("Creating the socket failed");
     return 1;
   }
-  //   struct sockadd serverSockAddr;
   struct sockaddr_in serverSockAddr;
   serverSockAddr.sin_family = AF_INET;
   serverSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -35,6 +47,9 @@ int main() {
       return 1;
     }
     printf("Client connected with %d\n", clientSocketfd);
+    char buffer[BUFFER_SIZE];
+    char method[16];
+    getMethod(clientSocketfd, buffer, method);
   }
   return 1;
 }
